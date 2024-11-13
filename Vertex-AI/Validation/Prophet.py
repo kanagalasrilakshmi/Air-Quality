e7e50f9918114f7105e025b10b45d1ab7766f6ad
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2024-11-13 02:36:55 -0500

    Update Prophet.py

--- Vertex-AI/Validation/Prophet.py
+++ Vertex-AI/Validation/Prophet.py
@@ -13,6 +13,11 @@ import mlflow.pyfunc
 import time
 import shap
 
+from google.cloud import storage
+import io
+from io import BytesIO
+import pickle5 as pickle
+
 
 class ProphetWrapper(mlflow.pyfunc.PythonModel):
     def __init__(self, prophet_model):
@@ -39,9 +44,23 @@ class ProphetPM25Model:
         self.y_test_original = None
     
     def load_data(self):
-        # Load training and test data
-        train_data = pd.read_pickle(self.train_file)
-        test_data = pd.read_pickle(self.test_file)
+        client = storage.Client()
+
+        # Specify your bucket name and the path to the pickle file in the 'processed' folder
+        bucket_name = 'airquality-mlops-rg'
+        pickle_file_path = 'processed/train/feature_eng_data.pkl'
+        pickle_file_path_test = 'processed/test/feature_eng_data.pkl'
+        # Get the bucket and the blob (file)
+        bucket = client.bucket(bucket_name)
+        blob_name = os.path.join(pickle_file_path)
+        blob = bucket.blob(blob_name)
+        pickle_data = blob.download_as_bytes() 
+        train_data = pickle.load(BytesIO(pickle_data))
+
+        blob_name_test = os.path.join(pickle_file_path_test)
+        blob_test = bucket.blob(blob_name_test)
+        pickle_data_test = blob_test.download_as_bytes() 
+        test_data = pickle.load(BytesIO(pickle_data_test))
 
         # Extract Box-Cox transformed y and original y
         for column in train_data.columns:
@@ -74,12 +93,13 @@ class ProphetPM25Model:
 
         # Calculate SHAP values for the test set
         shap_values = explainer(shap_data)
+        bucket_name = "airquality-mlops-rg"
 
         # Plot SHAP summary and save it as an artifact
         shap.summary_plot(shap_values, shap_data, show=False)
-        shap_plot_path = os.path.join(os.getcwd(), 'artifacts/shap_summary_plot_prophet.png')
+        shap_plot_path = f'gs://{bucket_name}/artifacts/shap_summary_plot_prophet.png'
         plt.savefig(shap_plot_path)
-        mlflow.log_artifact(shap_plot_path)
+        # mlflow.log_artifact(shap_plot_path)
         print(f"SHAP summary plot saved at {shap_plot_path}")
 
     def preprocess_data(self):
@@ -108,7 +128,7 @@ class ProphetPM25Model:
 
         # Evaluate RMSE on the original PM2.5 scale
         rmse_original = mean_squared_error(y_test_original_valid, y_pred_original_valid, squared=False)
-        mlflow.log_metric("RMSE",rmse_original)
+        #mlflow.log_metric("RMSE",rmse_original)
         print(f"RMSE (Original PM2.5 target): {rmse_original}")
 
         return y_pred_original_valid
@@ -128,46 +148,42 @@ class ProphetPM25Model:
         plt.xlabel('Time')
         plt.ylabel('PM2.5 Value')
         plt.legend()
-
+        bucket_name = "airquality-mlops-rg"
         # Save the plot as a PNG file
-        plot_path = os.path.join(os.getcwd(), 'artifacts/pm25_actual_vs_predicted_Prophet.png')
+        plot_path = f'gs://{bucket_name}/artifacts/pm25_actual_vs_predicted_Prophet.png'
         plt.savefig(plot_path)
-        mlflow.log_artifact(plot_path)
+        # mlflow.log_artifact(plot_path)
         print(f"Plot saved at {plot_path}")
 # Main function to orchestrate the workflow
 def main():
-    mlflow.set_experiment("PM2.5 Prophet")
-    curr_dir = os.getcwd()
-    print(curr_dir)
-    data_prepocessing_path_pkl = os.path.join(curr_dir,'DataPreprocessing/src/data_store_pkl_files')
+    bucket_name = "airquality-mlops-rg"
+    train_file_gcs = f'gs://{bucket_name}/processed/train/feature_eng_data.pkl'
+    test_file_gcs = f'gs://{bucket_name}/processed/test/feature_eng_data.pkl'
+    model_save_path_gcs = f'gs://{bucket_name}/weights/prophet_pm25_model.pth'
+    # mlflow.set_experiment("PM2.5 Prophet")
+    
     # Step 1: Load Data using DataFeatureEngineer
-    file_path = os.path.join(data_prepocessing_path_pkl, 'test_data/no_anamoly_test_data.pkl')
-    sys.path.append(data_prepocessing_path_pkl)
+    file_path = f'gs://{bucket_name}/processed/test/anamoly_data.pkl'
     from DataPreprocessing.src.test.data_preprocessing.feature_eng import DataFeatureEngineer
     engineer = DataFeatureEngineer(file_path)
     engineer.load_data()
     chosen_column = engineer.handle_skewness(column_name='pm25')
     engineer.feature_engineering(chosen_column)
     fitting_lambda = engineer.get_lambda()
-    mlflow.log_param("lambda_value", fitting_lambda)
-
-    # Step 2: Define file paths
-    train_file = os.path.join(data_prepocessing_path_pkl, 'train_data/feature_eng_train_data.pkl')
-    test_file = os.path.join(data_prepocessing_path_pkl, 'test_data/feature_eng_test_data.pkl')
-    model_save_path = os.path.join(curr_dir, 'weights/prophet_pm25_model.pth')
+    # mlflow.log_param("lambda_value", fitting_lambda)
     
-    if mlflow.active_run():
-        mlflow.end_run()
+    # if mlflow.active_run():
+    #     mlflow.end_run()
     
-    with mlflow.start_run() as run:
-        prophet_model = ProphetPM25Model(train_file, test_file, fitting_lambda, model_save_path)
-        prophet_model.load_data()
-        prophet_model.preprocess_data()
-        y_pred_original = prophet_model.evaluate()
-        prophet_model.shap_analysis()
-        prophet_model.load_weights()
-        prophet_model.plot_results(y_pred_original)
-    mlflow.end_run()
+    # with mlflow.start_run() as run:
+    prophet_model = ProphetPM25Model(train_file_gcs, test_file_gcs, fitting_lambda, model_save_path_gcs)
+    prophet_model.load_data()
+    prophet_model.preprocess_data()
+    y_pred_original = prophet_model.evaluate()
+    prophet_model.shap_analysis()
+    prophet_model.load_weights()
+    prophet_model.plot_results(y_pred_original)
+    # mlflow.end_run()
 if __name__ == "__main__":
     # path = "/Users/srilakshmikanagala/Desktop/Air/dags"
     # mlflow.set_tracking_uri("file:///opt/airflow/dags/mlruns")
