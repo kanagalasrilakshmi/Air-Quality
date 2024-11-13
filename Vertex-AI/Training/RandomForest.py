e223c6b62f0042cd73d7173bba4d148d1e21d9e8
kanagalasrilakshmi <116973326+kanagalasrilakshmi@users.noreply.github.com>
2024-11-13 01:48:26 -0500

    Update RandomForest.py

--- Vertex-AI/Training/RandomForest.py
+++ Vertex-AI/Training/RandomForest.py
@@ -10,6 +10,10 @@ import time
 import mlflow.sklearn
 import shap
 from sklearn.model_selection import GridSearchCV
+from google.cloud import storage
+import io
+from io import BytesIO
+import pickle5 as pickle
 
 class RandomForestPM25Model:
     def __init__(self, train_file, test_file, lambda_value, model_save_path):
@@ -33,7 +37,19 @@ class RandomForestPM25Model:
     
     def load_data(self):
         # Load training and test data
-        train_data = pd.read_pickle(self.train_file)
+        client = storage.Client()
+
+        # Specify your bucket name and the path to the pickle file in the 'processed' folder
+        bucket_name = 'airquality-mlops-rg'
+        pickle_file_path = 'processed/train/feature_eng_data.pkl'
+
+        # Get the bucket and the blob (file)
+        bucket = client.bucket(bucket_name)
+        blob_name = os.path.join(pickle_file_path)
+        blob = bucket.blob(blob_name)
+        pickle_data = blob.download_as_bytes() 
+        train_data = pickle.load(BytesIO(pickle_data))
+
         # Extract Box-Cox transformed y and original y
         for column in train_data.columns:
             if column == 'pm25_boxcox' or column == 'pm25_log':
@@ -49,51 +65,64 @@ class RandomForestPM25Model:
 
         # Log the best parameters and best RMSE
         best_params = grid_search.best_params_
-        mlflow.log_params(best_params)
+        #mlflow.log_params(best_params)
         print("Best parameters:", best_params)
         
         # Set the model to the best estimator
         self.model = grid_search.best_estimator_
 
         # Log the best model in MLflow
-        mlflow.sklearn.log_model(self.model, "XGBoost", input_example=self.X_train[:5])
+        #mlflow.sklearn.log_model(self.model, "XGBoost", input_example=self.X_train[:5])
     
-    def train_model(self):
-        # Train the model
-        self.model.fit(self.X_train, self.y_train)
-        mlflow.sklearn.log_model(self.model,"RandomForest",input_example=self.X_train[:5])
+    # def train_model(self):
+    #     # Train the model
+    #     self.model.fit(self.X_train, self.y_train)
+    #     mlflow.sklearn.log_model(self.model,"RandomForest",input_example=self.X_train[:5])
 
-        return y_pred_original
+    #     return y_pred_original
 
     def save_weights(self):
-        # Save the model weights to the specified path
-        model_save_path = self.model_save_path
-        with open(model_save_path, 'wb') as f:
-            pd.to_pickle(self.model, f)
-        mlflow.log_artifact(self.model_save_path)
-        print(f"Model saved at {model_save_path}")
+
+        storage_client = storage.Client()
+
+        # Define the bucket and the path to store the model weights
+        bucket_name = "airquality-mlops-rg"
+        model_blob_path = "weights/randomforest_pm25_model.pth"  # Path in the bucket where the model will be saved
+
+        # Get the bucket
+        bucket = storage_client.bucket(bucket_name)
+
+        # Create a blob object for the specified path
+        model_blob = bucket.blob(model_blob_path)
+
+        buffer = BytesIO()
+        pickle.dump(self.model, buffer)  # Serialize the model weights
+        buffer.seek(0)  # Ensure the buffer is at the start before uploading
+
+        # Upload the serialized model weights to GCS
+        model_blob.upload_from_file(buffer, content_type='application/octet-stream')
+
+        print(f"Model weights saved and uploaded to GCS at {model_blob_path}")
 
 def main():
-    mlflow.set_experiment("PM2.5 Random Forest")
-    curr_dir = os.getcwd()
-    data_prepocessing_path_pkl = os.path.join(curr_dir,'DataPreprocessing/src/data_store_pkl_files')
-    sys.path.append(data_prepocessing_path_pkl)
-    train_file = os.path.join(data_prepocessing_path_pkl, 'train_data/feature_eng_train_data.pkl')
-    model_save_path = os.path.join(curr_dir, 'weights/randomforest_pm25_model.pth')  # Save in .pth format
+    #mlflow.set_experiment("PM2.5 Random Forest")
+    bucket_name = "airquality-mlops-rg"
+    train_file_gcs = f'gs://{bucket_name}/processed/train/feature_eng_data.pkl'
+    model_save_path_gcs = f'gs://{bucket_name}/weights/randomforest_pm25_model.pth'
 
-    if mlflow.active_run():
-        mlflow.end_run()
+    # if mlflow.active_run():
+    #     mlflow.end_run()
 
-    with mlflow.start_run():
-        start_time = time.time()
-        rf_model = RandomForestPM25Model(train_file, None, None, model_save_path)
-        rf_model.load_data()
-        rf_model.grid_search_cv()
-        #rf_model.train_model()
-        train_duration = time.time() - start_time
-        mlflow.log_metric("training_duration", train_duration)
-        rf_model.save_weights()
-    mlflow.end_run()
+    # with mlflow.start_run():
+    start_time = time.time()
+    rf_model = RandomForestPM25Model(train_file_gcs, None, None, model_save_path_gcs)
+    rf_model.load_data()
+    rf_model.grid_search_cv()
+    #rf_model.train_model()
+    train_duration = time.time() - start_time
+    #mlflow.log_metric("training_duration", train_duration)
+    rf_model.save_weights()
+    # mlflow.end_run()
 
 if __name__ == "__main__":
     # mlflow.set_tracking_uri("file:///opt/airflow/dags/mlruns")
